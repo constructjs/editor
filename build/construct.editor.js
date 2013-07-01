bb089ca52a192a5b99305c0f97c71f98abb6566e
Makis Tracend <makis.tracend@gmail.com>
2013-07-01 04:37:59 -0700

    first build

---
+++ build/construct.editor.js
@@ -0,0 +1,71 @@
+(function( $ ){
+	// exit now if construct hasn't already been defined
+	if(typeof construct == "undefined") return;
+	
+	construct.editor = function( options ){
+		// initial setup
+		
+		construct.config.deps.push("dat.gui");
+		
+	}
+	
+	// Dependencies
+	construct.config = $.extend( true, construct.config, {
+		"paths": {
+			"dat.gui" : [
+				"//cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min"
+			]
+		}
+	});
+	
+})( jQuery );
+// Views
+(function( Backbone ){
+	
+	APP.Layouts.GUI = APP.Layout.extend({});
+	
+	APP.GUI = {};
+	
+	APP.GUI.Panel = APP.View.extend({
+		attributes : {},
+		initialize: function( options ){
+			_.bindAll(this, 'render', 'update', 'selections', 'save');
+			// every view has one instance of the gui
+			this.gui = new dat.GUI();
+			
+			this.attributes = this.collection.attributes;
+			if(options.resources) this.resources = options.resources;
+			
+			return APP.View.prototype.initialize.call(this, options);
+		}, 
+		render: function(){
+			// replace with your own:
+			// this.gui.add(...);
+		}, 
+		update : function(){
+			//broadbast changes to the gui
+			this.attributes = this.collection.attributes;
+		}, 
+		// create a object array from a collection
+		selections : function( items ){
+			var options = {};
+			for(var i in items){
+				// no id - no go...
+				if( !items[i].id) continue;
+				var id = items[i].id;
+				// lookup a descriptive name (fallback to the id)
+				var label = items[i].get("title") || items[i].get("name") || id;
+				options[label] = id;
+			}
+			return options;
+		}, 
+		save : function(){
+			//broadcast the changes back to the model
+			this.collection.set( this.attributes );
+			this.collection.save();
+		}
+			
+	});
+	
+	//APP.Views.
+})( this.Backbone );
